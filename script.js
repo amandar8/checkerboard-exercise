184e5d96f5c2e777824dcf3230d61068b81e796c
AMANDA RUTHERFOORD <AMANDARUTH08@GMAIL.COM>
2018-05-20 18:46:33 -0500

    non flashing checkboard

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// Your JS goes here
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+for (let i = 0; i < 81; i++) {
+  let tile = document.createElement('div');
+  let body = document.getElementsByTagName('body')[0];
+
+  tile.style.width = '11.1%';
+  tile.display = 'float';
+  tile.style.float = 'left';
+  tile.style.paddingBottom = '11.1%';
+
+  if (i % 2 === 0) {
+    tile.style.background = getRandomColor();
+  } else {
+    tile.style.background = getRandomColor();
+  }
+
+  body.appendChild(tile);
+
+}
+let flash = prompt("Dance Party!? (Y/N)").toLowerCase();
+if (flash === "y") {
+    setInterval(buildGrid, 200, 90, "random");
+}
+else {
+    let color = prompt("What colors would you like? (redAndBlack, random, or gradient)").toLowerCase();
+    buildGrid(90, color);
+}
